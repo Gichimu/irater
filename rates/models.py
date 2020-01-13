12392d34c281183919c123b24c1add79e093cf07
Gichimu <gichimueric12@gmail.com>
2020-01-13 19:44:43 +0300

    configure django allauth to send email and confirm registration

--- rates/models.py
+++ rates/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from pyuploadcare.dj.models import ImageField
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='images')
+    bio = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.bio
+
+class Project(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='images')
+    link = models.CharField(max_length=100)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+
--- rates/urls.py
+++ rates/urls.py
@@ -7,10 +7,18 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    url(r'^accounts/signup', SignupView.as_view(), name = 'account_signup'),
-    url(r'^accounts/login', views.login, name = 'account_login'),
+    # url(r'^accounts/signup', SignupView.as_view(), name = 'account_signup'),
+    # url(r'^accounts/login', views.login, name = 'account_login'),
     url(r'^accounts/changepassword', views.changePassword, name = 'change_password'),
+    url(r'^accounts/resetpassword', views.resetPassword, name = 'account_reset_password'),
+    url(r'^profile/(?P<user_id>\d+)', views.profile, name = 'profile'),
+    url(r'^edit/profile', views.update_profile, name = 'update_profile'),
+    url(r'^create/post', views.create_post, name = 'create_posts'),
+
 ] 
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
